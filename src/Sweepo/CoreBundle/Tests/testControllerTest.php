2eac86888c444e8084f0012383b8ee975b3a9638
Rémy GAZELOT <r.gazelot@gmail.com>
2012-12-17 15:37:36 +0100

    First test and travis.yml

---
+++ src/Sweepo/CoreBundle/Tests/testControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace mySweepo\CoreBundle\Tests;
+
+class testControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testA()
+    {
+        $this->assertTrue(true);
+    }
+}
